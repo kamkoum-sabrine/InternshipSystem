7e86c8e670636948f705bd4502b2c61aa2d32bfd
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2025-02-17 22:18:33 +0100

    create account user

--- Back/src/main/java/com/example/Back/Auth/Controllers/UserController.java
+++ Back/src/main/java/com/example/Back/Auth/Controllers/UserController.java
@@ -1,8 +1,13 @@
 package com.example.Back.Auth.Controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Back.Auth.Config.WebSecurityConfig;
 import com.example.Back.Auth.Models.User;
+import com.example.Back.Auth.Models.UserDTO;
 import com.example.Back.Auth.Repositories.RoleRepository;
 import com.example.Back.Auth.Repositories.UserRepository;
 
@@ -24,51 +30,100 @@ public class UserController {
     @Autowired
     RoleRepository roleRepository;
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @RequestMapping(value = "/register", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<Map<String, Object>> saveUser(@RequestBody UserDTO userDTO) {
+        System.out.println("Reçu : " + userDTO.getEmail()); // Debug
 
-    public ResponseEntity<JSONObject> saveUser(@RequestBody JSONObject user) {
+        if (userDTO.getEmail() == null) {
+            // JSONObject error = new JSONObject();
+            Map<String, Object> error = new HashMap<>();
+            error.put("message", "Missing field: email");
+            error.put("status", HttpStatus.BAD_REQUEST.value());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(error);
+        }
+        System.out.println("Vérification email : " + userDTO.getEmail());
+        System.out.println("Utilisateur trouvé : " + userRepository.findUserByEmail(userDTO.getEmail()));
 
+        if (userRepository.findUserByEmail(userDTO.getEmail()).isPresent()) {
+            // JSONObject error = new JSONObject();
+            Map<String, Object> error = new HashMap<>();
+            error.put("message", "email already exists");
+            error.put("status", HttpStatus.BAD_REQUEST.value());
+            System.out.println("Réponse d'erreur : " + error.toString());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(error);
+        }
         User appUser = new User();
-        user.get("email");
-        if (userRepository.findUserByEmail(user.get("email").toString()).isPresent()) {
-            JSONObject item = new JSONObject();
-            item.put("message", "email already exists");
-            item.put("status", HttpStatus.BAD_REQUEST.value());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(item);
-        }
+        appUser.setNom(userDTO.getNom());
+        appUser.setPrenom(userDTO.getPrenom());
+        appUser.setEmail(userDTO.getEmail());
+        String randomPassword = UUID.randomUUID().toString().replace("-",
+                "").substring(0, 8);
 
-        if (!user.get("password").toString().equals(user.get("confirmPassword").toString())) {
-            JSONObject item = new JSONObject();
-            item.put("message", "Please confirm Password");
-            item.put("status", HttpStatus.BAD_REQUEST.value());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(item);
-        }
-
-        appUser.setNom(user.get("nom").toString());
-        appUser.setPassword(WebSecurityConfig.passwordEncoder().encode(user.get("password").toString()));
-        appUser.setPrenom(user.get("prenom").toString());
-        appUser.setEmail(user.get("email").toString());
+        appUser.setPassword(randomPassword);
+        userDTO.setPassword(randomPassword);
+        // appUser.setPassword(WebSecurityConfig.passwordEncoder().encode(userDTO.getPassword()));
+        // // Sécurisé
         appUser.setActive(false);
-        appUser.setRole(roleRepository.findRoleByName(user.get("role").toString()));
+        appUser.setRole(roleRepository.findRoleByNom(userDTO.getRole()));
 
-        User newUser = userRepository.save(appUser);
-        // Optional<UserRole> userRole =
-        // userRoleRepository.findFirstByUserId(newUser.getId());
-        /**
-         * try {
-         * mailingService.sendVerificationEmail(newUser);
-         * } catch (Exception e) {
-         * JSONObject item = new JSONObject();
-         * item.put("message", e.getMessage());
-         * item.put("status", HttpStatus.BAD_REQUEST.value());
-         * e.printStackTrace();
-         * return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(item);
-         * }
-         */
-        JSONObject item = new JSONObject();
-        item.put("message", "Account");
-        item.put("user", userRepository.findUserByEmail(newUser.getEmail()).get());
-        return ResponseEntity.status(HttpStatus.CREATED).body(item);
+        userRepository.save(appUser);
 
+        // JSONObject success = new JSONObject();
+        Map<String, Object> success = new HashMap<>();
+        success.put("message", "Account created successfully");
+        success.put("user", userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(success);
     }
+
+    // @RequestMapping(value = "/register", method = RequestMethod.POST)
+
+    // public ResponseEntity<JSONObject> saveUser(@RequestBody User user) {
+
+    // User appUser = new User();
+    // // user.get("email");
+    // if (userRepository.findUserByEmail(user.getEmail().toString()).isPresent()) {
+    // JSONObject item = new JSONObject();
+    // item.put("message", "email already exists");
+    // item.put("status", HttpStatus.BAD_REQUEST.value());
+    // return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(item);
+    // }
+    // // String randomPassword = UUID.randomUUID().toString().replace("-",
+    // // "").substring(0, 12);
+    // //
+    // appUser.setPassword(WebSecurityConfig.passwordEncoder().encode(randomPassword));
+
+    // appUser.setNom(user.getNom().toString());
+    // appUser.setPassword(user.getPassword().toString());
+    // appUser.setPrenom(user.getPrenom().toString());
+    // appUser.setEmail(user.getEmail().toString());
+    // appUser.setActive(false);
+    // appUser.setRole(roleRepository.findRoleByNom(user.getRole().toString()));
+
+    // User newUser = userRepository.save(appUser);
+    // // Optional<UserRole> userRole =
+    // // userRoleRepository.findFirstByUserId(newUser.getId());
+    // /**
+    // * try {
+    // * mailingService.sendVerificationEmail(newUser);
+    // * } catch (Exception e) {
+    // * JSONObject item = new JSONObject();
+    // * item.put("message", e.getMessage());
+    // * item.put("status", HttpStatus.BAD_REQUEST.value());
+    // * e.printStackTrace();
+    // * return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(item);
+    // * }
+    // */
+    // JSONObject item = new JSONObject();
+    // item.put("message", "Account");
+    // item.put("user", userRepository.findUserByEmail(newUser.getEmail()).get());
+    // return ResponseEntity.status(HttpStatus.CREATED).body(item);
+
+    // }
 }
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -4,16 +4,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    TableModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ],
   bootstrap: [AppComponent]
 })
