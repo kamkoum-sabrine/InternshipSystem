d51cd433768d893f62971bd0bfa8b91f49cb5642
Mahdi-toumi <152063136+Mahdi-toumi@users.noreply.github.com>
2025-02-18 21:41:01 +0100

    Update web security authguard

--- Back/src/main/java/com/example/Back/Auth/Config/WebSecurityConfig.java
+++ Back/src/main/java/com/example/Back/Auth/Config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/auth/login", "api/auth/register", "api/auth/activate",
-                                "api/auth/desactivate")
+                                "api/auth/desactivate","/api/v1/soutenance","/api/v1/soutenance/search","/api/v1/enseingnant")
                         .permitAll() // Routes
                         // publiques
                         .anyRequest().authenticated())
