5d0a51f8bde6101889fa0f2663a5bf51ffef43c2
Mahdi-toumi <152063136+Mahdi-toumi@users.noreply.github.com>
2025-02-18 19:56:03 +0100

    Enseignant fonct

--- Back/src/main/java/com/example/Back/Soutenance/Controller/EnseignantController.java
+++ Back/src/main/java/com/example/Back/Soutenance/Controller/EnseignantController.java
@@ -14,6 +14,7 @@ public class EnseignantController {
 
     private final EnseignantService enseignantService;
 
+
     @Autowired
     public EnseignantController(EnseignantService enseignantService) {
         this.enseignantService = enseignantService;
