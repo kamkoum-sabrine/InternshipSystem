57144b208ba2b8f023166891b2ba9e6f235a1455
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2025-02-19 23:49:08 +0100

    create sidebar and change navbar

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -10,15 +10,33 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { EntreprisesComponent } from './pages/entreprises/entreprises.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarModule } from 'primeng/sidebar';
+import { MenuModule } from 'primeng/menu';
+import { AvatarModule } from 'primeng/avatar';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { PanelMenuModule } from 'primeng/panelmenu';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntreprisesComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
-    TableModule
+    TableModule,
+    BrowserAnimationsModule,
+    SidebarModule,
+    MenuModule,
+    AvatarModule,
+    OverlayPanelModule,
+    PanelMenuModule
   ],
   providers: [
     provideClientHydration(),
