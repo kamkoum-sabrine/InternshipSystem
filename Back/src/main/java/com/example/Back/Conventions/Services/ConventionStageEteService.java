5ca17942e3a4a155e5dffe95f2d94953bacaf5ef
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2025-04-02 15:32:18 +0100

    create ConventionStageEteService service

---
+++ Back/src/main/java/com/example/Back/Conventions/Services/ConventionStageEteService.java
@@ -0,0 +1,23 @@
+package com.example.Back.Conventions.Services;
+
+import com.example.Back.Auth.Models.User;
+import com.example.Back.Auth.Repositories.RoleRepository;
+import com.example.Back.Auth.Repositories.UserRepository;
+import com.example.Back.Conventions.Models.ConventionStageEte;
+import com.example.Back.Conventions.Repositories.ConventionStageEteRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConventionStageEteService {
+
+    private final ConventionStageEteRepository conventionStageEteRepository;
+
+    public ConventionStageEteService(ConventionStageEteRepository conventionStageEteRepository ) {
+        this.conventionStageEteRepository = conventionStageEteRepository;
+    }
+
+    public void saveConvetionStageEte(ConventionStageEte conventionStageEte) {
+        conventionStageEteRepository.save(conventionStageEte);
+    }
+
+}
