c4521da5d840ad33d76afdcaaf752b3d5686ed3c
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2025-04-03 19:36:57 +0100

    delete comments

--- Front/src/app/views/conventionsEtudiants/conventionsEtudiant-basic-example/gerer-conventionsEtudiant.service.ts
+++ Front/src/app/views/conventionsEtudiants/conventionsEtudiant-basic-example/gerer-conventionsEtudiant.service.ts
@@ -12,23 +12,6 @@ export class GererConventionsEtudiantService {
   constructor(private http: HttpClient) { }
 
   deposerConventionEtudiant(convention: any): Observable<any> {
-    // const formData = new FormData();
-    // formData.append('etudiantId', convention.etudiantId);
-    // formData.append('etablissement', convention.etablissement);
-    // formData.append('adresse', convention.adresse);
-    // formData.append('representePar', convention.representePar);
-    // formData.append('tuteurStage', convention.tuteurStage);
-    // formData.append('email', convention.email);
-    // formData.append('telephone', convention.telephone);
-    // formData.append('dateDebut', convention.dateDebut);
-    // formData.append('dateFin', convention.dateFin);
-    // const fichierPDF = convention.fichierPDF; // Récupérer le fichier
-    // if (fichierPDF instanceof Blob) {
-    //   formData.append('fichierPDF', fichierPDF, fichierPDF.name); // Ajouter le fichier correctement
-    // } else {
-    //   console.error('Le fichier n\'est pas valide', fichierPDF);
-    // }
-    // formData.append('fichierPDF', convention.fichierPDF, convention.fichierPDF.name);
     return this.http.post<any>(`${this.apiUrl}/conventionStagEte/create`, convention);
   }
   // desactiverCompte(id: number): Observable<any> {
