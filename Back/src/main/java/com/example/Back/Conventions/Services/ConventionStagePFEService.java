af4e46fbb1f96e6f006b233cc56520f9a6e9b1b1
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2025-04-12 10:47:38 +0100

    create ConventionStagePFEService

---
+++ Back/src/main/java/com/example/Back/Conventions/Services/ConventionStagePFEService.java
@@ -0,0 +1,18 @@
+package com.example.Back.Conventions.Services;
+
+import com.example.Back.Conventions.Models.ConventionStagePFE;
+import com.example.Back.Conventions.Repositories.ConventionStagePFERepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConventionStagePFEService {
+    private final ConventionStagePFERepository conventionStagePFERepository;
+
+    public ConventionStagePFEService(ConventionStagePFERepository conventionStagePFERepository ) {
+        this.conventionStagePFERepository = conventionStagePFERepository;
+    }
+
+    public void saveConvetionStagePFE(ConventionStagePFE conventionStagePFE) {
+        conventionStagePFERepository.save(conventionStagePFE);
+    }
+}
