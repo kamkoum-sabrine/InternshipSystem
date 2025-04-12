3f461147124dc37edcac949ed6ed5050dc3e832b
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2025-04-12 09:18:49 +0100

    create TuteurPFE repository

---
+++ Back/src/main/java/com/example/Back/Conventions/Repositories/TuteurPFERepository.java
@@ -0,0 +1,7 @@
+package com.example.Back.Conventions.Repositories;
+
+import com.example.Back.Conventions.Models.TuteurPFE;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TuteurPFERepository  extends JpaRepository<TuteurPFE, Long> {
+}
