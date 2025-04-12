40912629b5ad98b909a31797484588a9084cbf97
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2025-04-12 09:22:59 +0100

    create TuteurPFE service

---
+++ Back/src/main/java/com/example/Back/Conventions/Services/TuteurPFEService.java
@@ -0,0 +1,26 @@
+package com.example.Back.Conventions.Services;
+
+import com.example.Back.Conventions.Models.TuteurPFE;
+import com.example.Back.Conventions.Repositories.TuteurPFERepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TuteurPFEService {
+
+    private final TuteurPFERepository tuteurPFERepository;
+    @Autowired
+    public TuteurPFEService(TuteurPFERepository tuteurPFERepository ) {
+        this.tuteurPFERepository = tuteurPFERepository;
+    }
+
+    public void saveTuteurPFE(TuteurPFE tuteurPFE) {
+        tuteurPFERepository.save(tuteurPFE);
+    }
+
+    public List<TuteurPFE> getAllTuteurPFEs() {
+        return this.tuteurPFERepository.findAll();
+    }
+}
